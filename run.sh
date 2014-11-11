6a135555533332da0f8d6da8061f892c484f8794
rovingpen <rovingpen@users.noreply.github.com>
2014-11-11 01:17:43 -0600

    Updated script to ignore author's notes

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CHAPTERS=$(find . -iname '*.md' | tr '\n' ' ')
+CHAPTERS=$(find . -iname '*.md' -iname '*chapter*' | tr '\n' ' ')
 
 pandoc -s -S --toc -o book.epub title.txt $CHAPTERS
